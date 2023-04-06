09f1e955381758a77e9291b1fb02b0e35f282fba
Lukas <lukasasaelbacelar@gmail.com>
2023-04-06 12:17:07 -0300

    calculator UI implemented

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let num1;
 let num2;
 let operation;
+const symbols = [7, 4, 1, 0, 8, 5, 2, "", 9, 6, 3, "", "÷", "×", "-", "+", "Clear", "", "", "="];
 
 function operate(num1, num2, operation) {
     switch (operation) {
@@ -30,3 +31,22 @@ function multNum(a, b) {
 function divNum(a, b) {
     return (a / b);
 }
+
+function generateDivs() {
+    const calculator = document.querySelector('#calculator');
+    let counter = 0;
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 4; j++) {
+            const pixel = document.createElement('btn');
+            pixel.classList.add('pixel');
+            pixel.setAttribute("id", "pixel" + counter);
+            pixel.textContent = symbols[counter];
+            pixel.addEventListener('click', function handleClick(event) {
+                pixel.style.backgroundColor = "grey";
+            });
+            calculator.appendChild(pixel);
+            counter = counter + 1;
+        }
+    }
+}
+generateDivs()
