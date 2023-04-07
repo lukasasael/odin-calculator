a897789f7d4292d1b4c92e790fc52b81c42ff43b
Lukas <lukasasaelbacelar@gmail.com>
2023-04-07 16:53:24 -0300

    calculator funcional

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ let operation;
 const symbols = [7, 4, 1, 0, 8, 5, 2, "", 9, 6, 3, "", "÷", "×", "-", "+", "Clear", "", "", "="];
 
 function operate(num1, num2, operation) {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (!Number.isInteger(num1) || !Number.isInteger(num2))
+        return "Invalid Input";
     switch (operation) {
         case '+':
-            addNum(num1, num2)
+            return addNum(num1, num2);
         case '-':
-            subNum(num1, num2)
-        case '*':
-            multNum(num1, num2)
-        case '/':
-            divNum(num1, num2)
+            return subNum(num1, num2);
+        case '×':
+            return multNum(num1, num2);
+        case '÷':
+            return divNum(num1, num2);
+        default:
+            return "Invalid Input";
     }
 }
 
 function addNum(a, b) {
+
     return (a + b);
 }
 
@@ -29,11 +36,12 @@ function multNum(a, b) {
 }
 
 function divNum(a, b) {
-    return (a / b);
+    return (a / b).toFixed(2);
 }
 
 function generateDivs() {
     const calculator = document.querySelector('#calculator');
+    const display = document.querySelector('#display');
     let counter = 0;
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 4; j++) {
@@ -43,12 +51,26 @@ function generateDivs() {
             pixel.textContent = symbols[counter];
             pixel.style.backgroundColor = "#CDCDCD";
             pixel.addEventListener('click', function handleClick(event) {
-                pixel.style.backgroundColor = "grey";
+                display.textContent = `${display.textContent}${pixel.textContent}`;
             });
             calculator.appendChild(pixel);
             counter = counter + 1;
         }
     }
+    const clear = document.querySelector('#pixel16');
+    const equal = document.querySelector('#pixel19');
+
+    clear.addEventListener('click', function handleClick(event) {
+        display.textContent = "";
+    });
+    equal.addEventListener('click', function handleClick(event) {
+        let displayAll = display.textContent;
+        let displayNumbers = displayAll.split(/[^0-9\.]+/);
+        let displayOperator = displayAll.split("").filter(item => isNaN(item))[0];
+        display.textContent = operate(displayNumbers[0], displayNumbers[1], displayOperator);
+        console.log(operate(displayNumbers[0], displayNumbers[1], displayOperator));
+        //display.textContent = "";
+    });
 }
 
 generateDivs()
